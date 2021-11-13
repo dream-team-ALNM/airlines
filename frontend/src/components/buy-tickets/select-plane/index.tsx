ae31ec6f5ef4d697ed9e202f92edfca824e273bc
neonila-kunyk <n.kunyk-2025@kpi.ua>
2021-11-13 22:01:38 +0200

    Fixed aligment

--- frontend/src/components/buy-tickets/select-plane/index.tsx
+++ frontend/src/components/buy-tickets/select-plane/index.tsx
@@ -1,6 +1,7 @@
+import useWindowDimensions from 'hooks/window-dimensions.hook';
 import Select from 'react-select';
 import { IOption } from '../../../common/interfaces/components/option.interface';
-import selectPlaneStyles from './select-plane-styles';
+import getSelectPlaneStyles from './select-plane-styles';
 
 type Props = {
   handleSelectChange(selectedOption: IOption | null): void;
@@ -12,17 +13,22 @@ const SelectPlane: React.FC<Props> = ({
   handleSelectChange,
   options,
   placeholder,
-}) => (
-  <Select
-    className="mt-4 mb-5"
-    onChange={handleSelectChange}
-    options={options}
-    styles={selectPlaneStyles}
-    placeholder={placeholder}
-    isClearable
-    isSearchable
-    autoFocus
-  />
-);
+}) => {
+  const { width } = useWindowDimensions();
+  const selectWidth = width > 850 ? '17vw' : width > 600 ? '50vw' : '80vw';
+  const selectHeight = width > 850 ? '2.2vw' : width > 600 ? '4vw' : '6vw';
+  const selectFontSize = width > 850 ? '1.25vw' : width > 600 ? '2vw' : '3vw';
+  return (
+    <Select
+      onChange={handleSelectChange}
+      options={options}
+      styles={getSelectPlaneStyles(selectWidth, selectHeight, selectFontSize)}
+      placeholder={placeholder}
+      isClearable
+      isSearchable
+      autoFocus
+    />
+  );
+};
 
 export default SelectPlane;
